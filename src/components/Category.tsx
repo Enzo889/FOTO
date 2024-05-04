c327593efe49030ad83f693fe1714c917a89c86d
Enzo889 <enzobustamante895@gmail.com>
2024-05-04 19:05:06 -0300

    feat: update navbar

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -22,43 +22,56 @@ import {
 import { cn } from "@/lib/utils";
 
 export function Category() {
-  const components: { title: string; href: string; description: string }[] = [
+  const components: {
+    title: string;
+    href: string;
+    description: string;
+    icon: React.ReactElement;
+  }[] = [
     {
-      title: " Alert Dialog",
+      title: "Architecture",
       href: "/docs/primitives/alert-dialog",
       description:
-        "A modal dialog that interrupts the user with important content and expects a response.",
+        "Explore the world's iconic buildings and delve into architectural history and styles.",
+      icon: <Home />, // Asigna el ícono Home
     },
     {
-      title: "Hover Card",
+      title: "People",
       href: "/docs/primitives/hover-card",
       description:
-        "For sighted users to preview content available behind a link.",
+        "Connect with diverse cultures, discover inspiring stories, and celebrate human diversity.",
+      icon: <PersonStanding />, // Asigna el ícono PersonStanding
     },
     {
-      title: "Progress",
+      title: "Travel",
       href: "/docs/primitives/progress",
       description:
-        "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+        "Embark on virtual journeys, uncover hidden gems, and plan your dream vacation with our guides.",
+      icon: <Plane />, // Asigna el ícono Plane
     },
     {
-      title: "Scroll-area",
+      title: "Animals",
       href: "/docs/primitives/scroll-area",
-      description: "Visually or semantically separates content.",
+      description:
+        "Admire the wonders of the animal kingdom, learn about endangered species, and explore animal behavior.",
+      icon: <Rabbit />, // Asigna el ícono Rabbit
     },
     {
-      title: "Tabs",
+      title: "Nature",
       href: "/docs/primitives/tabs",
       description:
-        "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+        "Immerse yourself in the beauty of nature, understand ecosystems, and discover the importance of conservation.",
+      icon: <Sprout />, // Asigna el ícono Sprout
     },
     {
-      title: "Tooltip",
+      title: "Food",
       href: "/docs/primitives/tooltip",
       description:
-        "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+        "Indulge in a global culinary adventure, learn to cook authentic dishes, and discover food traditions.",
+      icon: <Utensils />, // Asigna el ícono Utensils
     },
   ];
+
   return (
     <NavigationMenu>
       <NavigationMenuList>
@@ -73,6 +86,7 @@ export function Category() {
                   key={component.title}
                   title={component.title}
                   href={component.href}
+                  icon={component.icon}
                 >
                   {component.description}
                 </ListItem>
@@ -85,28 +99,36 @@ export function Category() {
   );
 }
 
-const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            "block select-none space-y-1 rounded-lg p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground dark:hover:bg-neutral-800/50 dark:focus:bg-neutral-800/50 hover:bg-neutral-200 focus:bg-neutral-200 ",
-            className
-          )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none"> {title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
-    </li>
-  );
-});
+type ListItemProps = React.ComponentPropsWithoutRef<"a"> & {
+  title: string;
+  children?: React.ReactNode;
+  icon: React.ReactElement;
+};
+
+const ListItem = React.forwardRef<HTMLAnchorElement, ListItemProps>(
+  ({ className, title, children, icon, ...props }, ref) => {
+    return (
+      <li>
+        <NavigationMenuLink asChild>
+          <a
+            ref={ref}
+            className={cn(
+              "block select-none space-y-1 rounded-lg p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground dark:hover:bg-neutral-800/50 dark:focus:bg-neutral-800/50 hover:bg-neutral-200 focus:bg-neutral-200 ",
+              className
+            )}
+            {...props}
+          >
+            <div className="flex items-center space-x-2">
+              <div>{icon}</div>
+              <div className="text-sm font-medium leading-none"> {title}</div>
+            </div>
+            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+              {children}
+            </p>
+          </a>
+        </NavigationMenuLink>
+      </li>
+    );
+  }
+);
 ListItem.displayName = "ListItem";
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Input } from "./ui/input";
 
 function SearchInput() {
-  return <div>SearchInput</div>;
+  return (
+    <div className="flex w-full max-w-sm items-center space-x-2">
+      <Input
+        inputMode="search"
+        placeholder="Search images..."
+        className="rounded-xl "
+      />
+    </div>
+  );
 }
 
 export default SearchInput;
