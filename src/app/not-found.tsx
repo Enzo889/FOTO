23a13e0a73390219652666b7406fbbacdd8f6b2a
Enzo889 <enzobustamante895@gmail.com>
2024-07-21 22:00:21 -0300

    feat:add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,57 @@
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Not Found | FOTO",
+};
+function NotFoundPage() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-5">
+      <p>Sorry, this page was not found</p>
+      <p className="text-9xl">404</p>
+
+      <Link
+        href="/"
+        className="group relative inline-flex h-[calc(48px+8px)] items-center justify-center rounded-full bg-neutral-950 dark:bg-neutral-50 py-1 pl-6 pr-14 font-medium text-neutral-50 dark:text-neutral-950"
+      >
+        <span className="z-10 pr-2">Back to home</span>
+        <div className="absolute right-1 inline-flex h-12 w-12 items-center justify-end rounded-full  bg-neutral-700 dark:bg-neutral-600/65 transition-[width] group-hover:w-[calc(100%-8px)]">
+          <div className="mr-3.5 flex items-center justify-center">
+            <svg
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 text-neutral-50 dark:text-neutral-950"
+            >
+              <path
+                d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
+                fill="currentColor"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </div>
+        </div>
+      </Link>
+
+      <p className="absolute bottom-7 inline-flex gap-1">
+        made with{" "}
+        <span>
+          {" "}
+          <Image src="/heart.webp" alt="heart" width={24} height={24} />
+        </span>{" "}
+        by:{" "}
+        <span>
+          <Link target="_blank" href="https://twitter.com/enzodev_">
+            Enzo Bustamante
+          </Link>
+        </span>
+      </p>
+    </div>
+  );
+}
+
+export default NotFoundPage;
