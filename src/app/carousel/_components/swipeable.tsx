16aef547753aa9b734738379d1a0e641b74023cf
Enzo889 <enzobustamante895@gmail.com>
2024-07-28 00:02:53 -0300

    feat:fix mobile responsive

--- src/app/carousel/_components/swipeable.tsx
+++ src/app/carousel/_components/swipeable.tsx
@@ -118,7 +118,7 @@ export default function SwipeableStackCards() {
   };
 
   return (
-    <div className="flex flex-row-reverse justify-center gap-24 ">
+    <div className="flex flex-row-reverse justify-center max-md:flex-col-reverse max-md:gap-44  max-md:items-center gap-24  ">
       <div className="w-full max-w-xs p-4 relative  ">
         <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
         <div className="relative shadow-xl bg-gray-200 dark:bg-gray-900 border border-gray-400 dark:border-gray-800   px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start z-10">
